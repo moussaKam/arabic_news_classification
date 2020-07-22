bdfe30baaa9b9e41b6d3f6b407461490963f1773
moussaKam <moussa.kamaleddine@outlook.com>
2020-07-22 16:13:27 +0200

    minors

--- data_loader.py
+++ data_loader.py
@@ -1,10 +1,12 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
+import numpy
 
 class NewsDataset(Dataset):
-    def __init__(self, path_documents, path_labels, path_dict, max_len=512):
+    def __init__(self, path_documents, path_labels, word2ind, max_len=512):
         self.max_len = max_len
+        self.word2ind = word2ind
         self.documents = []
         self.labels = []
         with open(path_documents, 'r') as f1, open(path_labels, 'r') as f2:
@@ -13,20 +15,13 @@ class NewsDataset(Dataset):
             for line in f2:
                 self.labels.append(int(line.strip()))
             assert len(self.documents) == len(self.labels)
-            
-        self.dict = {}
-        with open(path_dict, 'r') as f:
-            for line in f:
-                tmp = line.split()
-                self.dict[tmp[0]] = tmp[1]
-            f.close()
-                
+                    
         def __len__(self):
             return len(documents)
         
         def __getitem__(self, index):
             sequence = self.documents.split()
-            sequence = sequence[:max_len]
+            sequence = [word2ind[word] for word in sequence[:max_len]]
             sample = {'sequence': torch.tensor(sequence), 'label': labels[index]}
             return sample
         
@@ -36,22 +31,26 @@ class NewsDataset(Dataset):
                              dtype=torch.long)
         return sequences, labels
     
-    def get_loader(path_documents, path_labels, path_dict, max_len=512, batch_size=16):
+    def get_loader(path_documents, path_labels, word2ind, device='cpu' max_len=512, batch_size=16):
         """
         Args:
             path_documents: path to file containing documents.
             path_labels: path to file containing labels
+            word2ind: dictionnary mapping words to their respective index
+            device: string: 'cpu' of 'cuda'
             batch_size: mini-batch size.
         Returns:
             data_loader: data loader for custom dataset.
         """
         dataset = NewsDataset(path_documents, path_labels, path_dict, max_len=512)
+        device=device.lower()
+        assert (device=='cpu' or device=='cuda')
         data_loader = torch.utils.data.DataLoader(dataset=dataset,
                                                   batch_size=batch_size,
                                                   shuffle=True,
                                                   collate_fn=collate_fn,
                                                   drop_last=True,
-                                                  pin_memory=True
+                                                  pin_memory=(device=='cuda')
                                                   )
 
         return data_loader
