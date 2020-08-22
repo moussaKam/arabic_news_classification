f14068c2a1412544cd5b39f224bf50d7ffd00a9d
moussaKam <moussa.kamaleddine@outlook.com>
2020-08-22 13:46:29 +0200

    fix collate function

--- data_loader.py
+++ data_loader.py
@@ -24,12 +24,25 @@ class NewsDataset(Dataset):
         sequence = [self.word2ind[word] for word in sequence[:self.max_len]]
         sample = {'sequence': torch.tensor(sequence), 'label': self.labels[index]}
         return sample
+    
+class MyCollator(object):
+    def __init__(self, device, pad_to_maxlen=False):
+        """
+        Args:
+            device: cpu or cuda
+            pad_to_maxlen: if True always pad to max length else pad to max sequence length within batch
+        """
+        self.device = device
+        self.pad_to_maxlen = pad_to_maxlen
         
-def collate_fn(data):
-    sequences = pad_sequence([sample['sequence'] for sample in data])
-    labels = torch.tensor([sample['label'] for sample in data],
-                            dtype=torch.long)
-    return sequences, labels
+    def __call__(self, batch):
+        if not self.pad_to_maxlen: # rnn
+            sequences = pad_sequence([sample['sequence'] for sample in batch])
+            labels = torch.tensor([sample['label'] for sample in batch],
+                                    dtype=torch.long)
+            return sequences.to(self.device), labels.to(self.device)
+        else: #cnn
+            pass
     
 def get_loader(path_documents, path_labels, word2ind, device='cpu', max_len=512, batch_size=16):
         """
@@ -41,16 +54,16 @@ def get_loader(path_documents, path_labels, word2ind, device='cpu', max_len=512,
             batch_size: mini-batch size.
         Returns:
             data_loader: data loader for custom dataset.
-            """
+        """
+        collator = MyCollator(device)
         dataset = NewsDataset(path_documents, path_labels, word2ind, max_len=512)
         device=device.lower()
         assert (device=='cpu' or device=='cuda')
         data_loader = DataLoader(dataset=dataset,
                                                   batch_size=batch_size,
                                                   shuffle=False,
-                                                  collate_fn=collate_fn,
+                                                  collate_fn=collator,
                                                   drop_last=False,
-                                                  pin_memory=(device=='cuda')
                                                   )
 
         return data_loader
